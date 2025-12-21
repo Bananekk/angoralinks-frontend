fca80f40a8d0099fecea80b8f39344a42bf33a46
iobywatel <mnich.mniszkowy24@gmail.com>
2025-12-21 20:59:01 +0100

    fix baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://angoralinks-backend.onrender.com',
+  baseURL: 'https://angoralinks-backend.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
