d0ab44463ca073f9839fb8168fbbb4178e7c640e
iobywatel <mnich.mniszkowy24@gmail.com>
2026-01-11 20:45:17 +0100

    Fix verify

---
+++ src/pages/_document.jsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head>
+        <meta name="monetag" content="e86da4741d360ad921f3920fb97947fa" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
